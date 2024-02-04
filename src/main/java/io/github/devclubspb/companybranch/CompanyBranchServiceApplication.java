91165ff7cbf56650fbe08ef7b02da7e83ee903cf
Grig Alex <alexengrigdev@gmail.com>
2024-02-04 14:57:32 +0300

    Init commit

---
+++ src/main/java/io/github/devclubspb/companybranch/CompanyBranchServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.devclubspb.companybranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyBranchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyBranchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/devclubspb/companybranch/CompanyBranchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devclubspb.companybranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyBranchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
